543c87525c833be8626e3b36d4d7af567e9022a7
Alvaro Valdebenito <alvarov@met.no>
2024-01-04 12:45:57 +0100

    use importlib_metadata on Python 3.9

--- src/pyaro/plugins.py
+++ src/pyaro/plugins.py
@@ -2,7 +2,11 @@ import functools
 import sys
 import warnings
 
-from importlib.metadata import EntryPoints, entry_points
+if sys.version_info >= (3, 10):
+    from importlib.metadata import EntryPoints, entry_points
+else:
+    from importlib_metadata import EntryPoints, entry_points
+
 from .timeseries.Engine import Engine as TimeseriesEngine
 from .timeseries.Reader import Reader as TimeseriesReader
 
@@ -41,10 +45,7 @@ def list_timeseries_engines() -> dict[str, TimeseriesEngine]:
 
     # New selection mechanism introduced with Python 3.10. See GH6514.
     """
-    if sys.version_info >= (3, 10):
-        entrypoints = entry_points(group="pyaro.timeseries")
-    else:
-        entrypoints = entry_points().get("pyaro.timeseries", [])
+    entrypoints = entry_points(group="pyaro.timeseries")
     return build_timeseries_engines(entrypoints)
 
 
